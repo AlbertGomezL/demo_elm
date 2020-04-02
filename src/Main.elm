e2547b202f57151cc4ab563fb404de11862b08ad
Alberto Gómez <albertogomezleon04@gmail.com>
2020-04-02 16:42:08 +0200

    Add CSS

--- src/Main.elm
+++ src/Main.elm
@@ -28,6 +28,11 @@ initModel =
     , newProduct 4 "Martillo" 7.0
     , newProduct 2 "Tornillos" 4.2
     , newProduct 3 "Rastrillo" 10
+    , newProduct 5 "Destornillador" 3.4
+    , newProduct 7 "Pala" 12
+    , newProduct 8 "Alicates" 4
+    , newProduct 6 "Llave" 6.3
+    , newProduct 9 "Taladro" 22.5
     ]
 
 
@@ -63,22 +68,40 @@ totalCost products =
 
 pageHeader : Html msg
 pageHeader = 
-    h1 [] [text "App Products"]
+    h1  [style "background-color" "darkcyan"
+        , style "margin-top" "0px"
+        , style "height" "100px"
+        , style "text-align" "center"
+        , style "padding-top" "50px"
+        ] [text "Lista de productos"]
 
 pageFooter : Html msg
 pageFooter = 
     footer [] [
-        a [href "https://www.google.es"]
-          [text "Aplicación de compra de products"]
+        a [href "https://github.com/AlbertGomezL/demo_elm.git"]
+          [text "Repositorio de código"]
     ]
 
 article : Product -> Html Msg
 article art = 
-    li [] 
-    [ span [class "name"] [text art.name]
-    , span [class "cost"] [text (String.fromFloat art.cost)]
-    , button [class "delete", onClick (Delete art.id)] [text "x"]
-    ]
+    li 
+        [ style "height" "40px"
+        , style "padding-top" "20px"
+        , style "margin-left" "20%"
+        , style "margin-right" "20%"
+        , style "margin-bottom" "10px"
+        , style "border-radius" "15px"
+        , style "background-color" "lightblue"
+        ] 
+        [ span [class "name", style "margin-left" "10%"] [text art.name]
+        , span [class "cost", style "margin-left" "25%"] [text (String.fromFloat art.cost), text " €"]
+        , button [class "delete"
+                 , style "position" "absolute"
+                 , style "right" "25%"
+                 , onClick (Delete art.id)
+                 ] 
+                 [text "x"]
+        ]
 
 articles : List Product -> Html Msg
 articles products =
@@ -86,38 +109,47 @@ articles products =
         entries = List.map article products
         elements = entries ++ [showTotal (totalCost products)]
     in
-        ul [] elements
+        ul [style "margin-left" "50px", style "margin-right" "50px"] elements
 
 showTotal : Float -> Html msg
 showTotal total =
-    li [class "total"]
-        [ span [class "label"] [text "Total"]
-        , span [class "cost"] [text (String.fromFloat total)]
+    li 
+        [ class "total"
+        , style "height" "40px"
+        , style "padding-top" "20px"
+        , style "margin-left" "20%"
+        , style "margin-right" "20%"
+        , style "margin-bottom" "10px"
+        , style "border-radius" "15px"
+        , style "background-color" "mediumspringgreen"
+        ]
+        [ span [class "label", style "margin-left" "10%"] [text "Precio total"]
+        , span [class "cost", style "margin-left" "25%"] [text (String.fromFloat total), text " €"]
         ]
 
 view : Model -> Html Msg
 view model = 
     div [id "container"] 
         [ pageHeader
-        , button
-            [class "sort left", onClick SortByNombre]
-            [text "Nombre"]
-        , button
-            [class "sort", onClick SortByPrecio]
-            [text "Precio"]
+        , div[style "text-align" "center"] 
+            [ span [] [text "Ordenar por "]
+            , button
+                [class "sort left", onClick SortByNombre]
+                [text "Nombre"]
+            , button
+                [class "sort", onClick SortByPrecio]
+                [text "Precio"]
+            ]
         , articles model
         , pageFooter
         ]
 
 
--- main : Html msg
+-- MAIN
 main = 
     Browser.sandbox 
     { init = update SortByNombre initModel
     , view = view
     , update = update
     }
-    -- view initModel
-    -- Html.text "Hola mundo!"
-
 
